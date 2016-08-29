d034292b9851b0b53c575dcc5e347c22b5505bef
HomerIce <homerice8@gmail.com>
2016-08-29 11:07:16 +0800

    创建项目文件

---
+++ WeiBo/WeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeiBo
+//
+//  Created by HomerIce on 16/8/29.
+//  Copyright © 2016年 HomerIce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
